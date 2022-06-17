8ce02fec1172fabea2e822b8af12e340f1732e91
Anirudh Loona <78737647+aniloona31@users.noreply.github.com>
2022-06-17 11:20:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,24 +21,3 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBUDJ9aPVunedHMKVhoFLAFvqG1AUrDuKw",
-//   authDomain: "clone-6050a.firebaseapp.com",
-//   projectId: "clone-6050a",
-//   storageBucket: "clone-6050a.appspot.com",
-//   messagingSenderId: "431428155990",
-//   appId: "1:431428155990:web:281f122aed0e2dcae499f7",
-//   measurementId: "G-BNERTNQB5R"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
